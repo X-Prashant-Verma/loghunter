1b0741c10bc845ca49e7499f27b32a7c6447a505
Prashant Verma <prashantverma028@gmail.com>
2026-03-26 23:01:32 +0530

    add error handling throughout

--- loghunter.sh
+++ loghunter.sh
@@ -19,6 +19,11 @@ current_time=$(date +"%Y-%m-%d_%H-%M")
 
 sorted_report=$( grep -i "$severity_level" "$file_path" | awk -F' ' '{$1=$2=$3=""; print $0}' | sort | uniq -c | sort -rn ) 
 
-echo "$sorted_report" > ./report-files/loghunter_report_$current_time.txt
+if [ -z "$sorted_report" ]
+then 
+    echo "No $severity_level Found in the logs"
+else
+    echo "$sorted_report" > ./report-files/loghunter_report_$current_time.txt
+fi
 
 
